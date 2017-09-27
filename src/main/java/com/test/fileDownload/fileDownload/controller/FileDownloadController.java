488240419edae453ab28aa27d1b26a69a51bde16
Amar Singh <amarsingh@outlook.in>
2017-09-28 01:01:32 +0530

    Amar | Finally did it \!

--- src/main/java/com/test/fileDownload/fileDownload/controller/FileDownloadController.java
+++ src/main/java/com/test/fileDownload/fileDownload/controller/FileDownloadController.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.apache.commons.codec.binary.Base64;
 
@@ -12,137 +14,96 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = { "http://localhost:8081/","http://localhost:8081" }, maxAge = 3600)
 @RequestMapping(value = "/file")
 public class FileDownloadController {
-	public static int BUFFER_SIZE = 1024;
+	public static int BUFFER_SIZE = 1026;
+	public static String FILE_PATH = "/Users/amar/Downloads/a.pdf";
+	public static String LARGE_FILE_NAME = "/Users/amar/Downloads/The.Boss.Baby.mkv";
+
+	@RequestMapping(value = "/downloadViaGet", method = RequestMethod.GET, produces = {
+			MediaType.APPLICATION_OCTET_STREAM_VALUE })
+	public void downloadFileViaGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		File file = new File(FILE_PATH);
+
+		try (OutputStream out = response.getOutputStream()) {
+			Path path = file.toPath();
+			Files.copy(path, out);
+		} catch (IOException e) {
+			throw e;
+		}
+	}
+
+	@RequestMapping(value = "/downloadViaPost", method = RequestMethod.POST, produces = {
+			MediaType.APPLICATION_OCTET_STREAM_VALUE })
+	public void downloadFileViaPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		File file = new File(FILE_PATH);
+
+		try (OutputStream out = response.getOutputStream()) {
+			Path path = file.toPath();
+			Files.copy(path, out);
+		} catch (IOException e) {
+			throw e;
+		}
+	}
+
 	@RequestMapping(value = "/download", method = RequestMethod.POST)
-	public void downloadFile(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		ServletOutputStream outputStream = response.getOutputStream();
-		FileInputStream input = new FileInputStream("/Users/amar/Downloads/AmarSingh.pdf");
+	public void doDownload(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-		response.setContentType("application/pdf");
-		int c = 0;
-		byte[] data = new byte[1024];
-		/*
-		 * while((c = input.read()) != -1){ Base64.encodeInteger(c);
-		 * outputStream.write(c); }
-		 */
+		// get absolute path of the application
+		ServletContext context = request.getServletContext();
+		String appPath = context.getRealPath("");
+		System.out.println("appPath = " + appPath);
 
-		while ((c = input.read(data, 0, data.length)) != -1) {
-			String encodeBase64String = Base64.encodeBase64String(data);
-			//outputStream.write(data, 0, c);
-			outputStream.write(data, 0, c);
+		// construct the complete absolute path of the file
+		// String fullPath = appPath + filePath;
+		File downloadFile = new File(LARGE_FILE_NAME);
+		FileInputStream inputStream = new FileInputStream(downloadFile);
+
+		// get MIME type of the file
+		String mimeType = context.getMimeType(LARGE_FILE_NAME);
+		if (mimeType == null) {
+			// set to binary type if MIME mapping not found
+			mimeType = "application/octet-stream";
+		}
+		System.out.println("MIME type: " + mimeType);
+
+		// set content attributes for the response
+		response.setContentType(mimeType);
+		response.setContentLength((int) downloadFile.length());
+
+		// set headers for the response
+		String headerKey = "Content-Disposition";
+		String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
+		response.setHeader(headerKey, headerValue);
+
+		// get output stream of the response
+		OutputStream outStream = response.getOutputStream();
+
+		byte[] buffer = new byte[BUFFER_SIZE];
+		int bytesRead = -1;
+
+		// write bytes read from the input stream into the output stream
+		int output = 0;
+		while ((bytesRead = inputStream.read(buffer)) != -1) {
+
+			outStream.write(buffer, 0, bytesRead);
+			// for(byte b : buffer) {
+			// System.out.print(b + ",");
+			// }
+			output += buffer.length;
 		}
 		
-		outputStream.flush();
-		outputStream.close();
+		inputStream.close();
+		outStream.close();
+
 	}
-	
-	
-	@RequestMapping(value = "/download2", method = RequestMethod.POST)
-    public void doDownload(HttpServletRequest request,
-            HttpServletResponse response) throws IOException {
- 
-        // get absolute path of the application
-        ServletContext context = request.getServletContext();
-        String appPath = context.getRealPath("");
-        System.out.println("appPath = " + appPath);
- 
-        // construct the complete absolute path of the file
-        //String fullPath = appPath + filePath;      
-        File downloadFile = new File("/Users/amar/Downloads/AmarSingh.pdf");
-        FileInputStream inputStream = new FileInputStream(downloadFile);
-         
-        // get MIME type of the file
-        String mimeType = context.getMimeType("/Users/amar/Downloads/AmarSingh.pdf");
-        if (mimeType == null) {
-            // set to binary type if MIME mapping not found
-            mimeType = "application/octet-stream";
-        }
-        System.out.println("MIME type: " + mimeType);
- 
-        // set content attributes for the response
-        response.setContentType(mimeType);
-        response.setContentLength((int) downloadFile.length());
- 
-        // set headers for the response
-        String headerKey = "Content-Disposition";
-        String headerValue = String.format("attachment; filename=\"%s\"",
-                downloadFile.getName());
-        response.setHeader(headerKey, headerValue);
- 
-        // get output stream of the response
-        OutputStream outStream = response.getOutputStream();
- 
-        byte[] buffer = new byte[BUFFER_SIZE];
-        int bytesRead = -1;
- 
-        // write bytes read from the input stream into the output stream
-        while ((bytesRead = inputStream.read(buffer)) != -1) {
-            outStream.write(buffer, 0, bytesRead);
-            for(byte b : buffer) {
-            	System.out.print(b + ",");
-            }
-        }
- 
-        inputStream.close();
-        outStream.close();
- 
-    }
-	
-	@RequestMapping(value = "/download2", method = RequestMethod.GET)
-    public void doDownloadGet(HttpServletRequest request,
-            HttpServletResponse response) throws IOException {
- 
-        // get absolute path of the application
-        ServletContext context = request.getServletContext();
-        String appPath = context.getRealPath("");
-        System.out.println("appPath = " + appPath);
- 
-        // construct the complete absolute path of the file
-        //String fullPath = appPath + filePath;      
-        File downloadFile = new File("/Users/amar/Downloads/AmarSingh.pdf");
-        FileInputStream inputStream = new FileInputStream(downloadFile);
-         
-        // get MIME type of the file
-        String mimeType = context.getMimeType("/Users/amar/Downloads/AmarSingh.pdf");
-        if (mimeType == null) {
-            // set to binary type if MIME mapping not found
-            mimeType = "application/octet-stream";
-        }
-        System.out.println("MIME type: " + mimeType);
- 
-        // set content attributes for the response
-        response.setContentType(mimeType);
-        response.setContentLength((int) downloadFile.length());
- 
-        // set headers for the response
-        String headerKey = "Content-Disposition";
-        String headerValue = String.format("attachment; filename=\"%s\"",
-                downloadFile.getName());
-        response.setHeader(headerKey, headerValue);
- 
-        // get output stream of the response
-        OutputStream outStream = response.getOutputStream();
- 
-        byte[] buffer = new byte[BUFFER_SIZE];
-        int bytesRead = -1;
- 
-        // write bytes read from the input stream into the output stream
-        while ((bytesRead = inputStream.read(buffer)) != -1) {
-            outStream.write(buffer, 0, bytesRead);
-            System.out.println(buffer);
-        }
- 
-        inputStream.close();
-        outStream.close();
- 
-    }
+
 }
